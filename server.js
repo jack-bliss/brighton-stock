e251672e7d0d88a42f9e99c06c73c8317805382e
Jack Bliss <j.bliss@campussociety.com>
2017-08-15 17:28:00 +0100

    Added redirects for league and facebook

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const port = process.env.PORT || 3001;
 
 app.get('/stats', (req, res) => {
     res.redirect('http://jackbliss.co.uk/brighton-glicko');
-})
+});
+app.get('/league', (req, res) => {
+    res.redirect('https://platinum-smash.herokuapp.com/players');
+});
+app.get('/fb', (req, res) => {
+    res.redirect('https://www.facebook.com/BrightonStockSmash/');
+});
 
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/dist/index.html');
