37beefd9640edda6b8d4e84c3216820a390e00f4
Jack Bliss <j.bliss@campussociety.com>
2017-08-16 12:32:28 +0100

    Still trying to figure out wwwhisper

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const redis = require('redis').createClient(process.env.REDIS_URL);
 const wwwhisper = require('connect-wwwhisper');
 
 app.use('/admin_page', wwwhisper());
-app.use('/wwwhisper', wwwhisper());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './dist')));
 
